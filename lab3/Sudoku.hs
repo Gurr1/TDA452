da94c31bceb68b2c9594f049239d5ac436b55f1f
Gustav Engsmyre <gustav.engsmyre@gmail.com>
2018-12-05 14:23:12 +0100

    finish solve

--- lab3/Sudoku.hs
+++ lab3/Sudoku.hs
@@ -2,8 +2,9 @@ module Sudoku where
 
 import Data.Char
 import Test.QuickCheck
-import Data.Maybe (isNothing)
 import Data.List
+import Data.Maybe
+
 data Sudoku = Sudoku { rows :: [[Maybe Int]] }
     deriving (Eq, Show)
 example :: Sudoku
@@ -96,17 +97,126 @@ type Block = [Maybe Int]
   
   
   -- * D1
+
+  
+  --TODO 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  -- * D2
+  
+-- Checks if the given block is valid according to sudoku rules
   
 isOkayBlock :: Block -> Bool
 isOkayBlock [] = True
 isOkayBlock (Nothing:xs) = isOkayBlock xs
 isOkayBlock (x:xs) = not (elem x xs) && (isOkayBlock xs)
   
-  
-  -- * D2
-  
+  -- Returns the rows, columns and blocks given a sudoku
 blocks :: Sudoku -> [Block]
-blocks b= getRows b ++ getColumns b ++ squareBlock (transpose (extractRow b))
+blocks b = getRows b ++ getColumns b ++ squareBlock (transpose (extractRow b))
 
 getRows :: Sudoku -> [Block]
 getRows = extractRow
@@ -123,12 +233,22 @@ squareBlock' :: [Block] -> [Block]
 squareBlock' (row1 : row2 : row3 : em)
   | null row1 = []
   | otherwise = (take 3 row1 ++ take 3 row2 ++ take 3 row3) :
-        squareBlock' (drop 3 row1 : drop 3 row2 : drop 3 row3 : em)
-  
+  squareBlock' (drop 3 row1 : drop 3 row2 : drop 3 row3 : em)
+    
 prop_blocks_lengths :: Sudoku -> Bool
 prop_blocks_lengths sudoku = hasCorrectnBlocks e 
                             && and [hasCorrectCells a | a <- e]
-                            where e = extractRow sudoku
+    where e = extractRow sudoku
+
+-- Checks if all blocks in a sudoku are valid
+isOkay:: Sudoku -> Bool
+isOkay s = all isOkayBlock (blocks s)
+
+-- Property to validate the amount of blocks and their respective sizes
+prop_blocks :: Sudoku -> Bool
+prop_blocks s = length blocks' == 27 && all (\b -> length b == 9) blocks'
+    where blocks' = blocks s
+  
 
 hasCorrectnBlocks :: [Block] -> Bool
 hasCorrectnBlocks blocks = length blocks == 9
@@ -138,8 +258,6 @@ hasCorrectCells block = length block == 9
 
 -- * D3
 
-isOkay :: Sudoku -> Bool
-isOkay b = all isOkayBlock (blocks b)
   
   
   ---- Part A ends here --------------------------------------------------------
@@ -155,24 +273,97 @@ type Pos = (Int,Int)
   
 --blanks :: Sudoku -> [Pos]
 --blanks (Sudoku sudoku) = [(0,a) | a <- sudoku !! 0] 
+blanks :: Sudoku -> [Pos]
+--generates a pos pair for each row with the cordinate, then each tile in the row if the cell is nothing
+blanks s = [(y,x) | (y, row) <- zip [0..] (rows s), 
+                    (x, cell) <- zip [0..] row, 
+                    isNothing cell]
+
+hasValue :: Sudoku -> Pos -> Maybe Int
+hasValue sudoku (y, x) = ((extractRow sudoku) !! y) !! x
+
+prop_blanks_allBlank :: Sudoku -> Bool
+prop_blanks_allBlank sudoku = and [(hasValue sudoku b) == Nothing | b <- blankSpots]
+  where
+    blankSpots = blanks sudoku
+
   
+
+--nothingAt :: Sudoku -> [Pos]  -> Bool
+--nothingAt a b = sudoku 
   --prop_blanks_allBlanks :: ...
   --prop_blanks_allBlanks =
   
   
   -- * E2
-  
+--Replaces position i with value y and returns a copy of the list
 (!!=) :: [a] -> (Int,a) -> [a]
-xs !!= (i,y) | i > length xs || i < 0 = error "index out of bounds"
+xs !!= (i,y) | i >= length xs || i < 0 = error "index out of bounds"
+[] !!= (i,y) = []
 xs !!= (i,y) = l1 ++ (y : tail l2)
               where (l1,l2) = (splitAt i xs)
 
 -- Finish Writing these
-prop_bangBangEquals_correct :: [a] -> Bool
-prop_bangBangEquals_correct xs = hasCorrectLength 
+
+validIndex :: Int -> [a] -> Bool
+validIndex i [] = False
+validIndex i s | i < 0 || length s <= i = False
+validIndex i s | otherwise = True
+
+prop_bangBangEquals_correct :: Integral a => [a] -> (Int, a)-> Property   --FIX THESE TESTS
+prop_bangBangEquals_correct xs (i,y) = validIndex i xs ==> hasCorrectLength xs (xs !!= (i, y))
 
 hasCorrectLength :: [a] -> [a] -> Bool
 hasCorrectLength list1 list2 = length list1 == length list2
 
-replacesValue :: (Int, a) -> [a] -> Bool
-replacesValue (i, y) list = (list !! i) == y
+replacesValue :: Integral a => (Int, a) -> [a] -> Bool
+replacesValue (i, y) list = ((list !!= (i, y)) !! i) == y
+
+--E3
+
+update :: Sudoku -> Pos -> Maybe Int -> Sudoku
+update sudoku (x, y) value = Sudoku (extractRow sudoku !!= (x, a !!= (y, value)))
+                                      where a = extractRow sudoku !! x
+
+validPosition :: Pos -> Bool
+validPosition (x, y) | (x < 0 && x > 8) && (y < 0 && y > 8) = False
+                      | otherwise = True
+
+prop_update_updated :: Sudoku -> Pos -> Maybe Int -> Property
+prop_update_updated sudoku pos value = validPosition ==> hasValue (update sudoku pos value) pos == value
+
+
+-- E4
+candidates :: Sudoku -> Pos -> [Maybe Int]
+candidates s p = filter (cand s p) (map Just [1..9])
+
+cand :: Sudoku -> Pos -> Maybe Int -> Bool
+cand s p n | isOkay(update s p n) = True
+           | otherwise = False
+
+solve :: Sudoku -> Maybe Sudoku
+solve sudoku = solve' sudoku (blanks sudoku) 
+
+
+solve' :: Sudoku -> [Pos] -> Maybe Sudoku 
+solve' sudoku [] | not (isOkay sudoku) || not (isSudoku sudoku) = Nothing
+                 | otherwise = Just sudoku
+solve' sudoku (p:ps) = listToMaybe (catMaybes newSudoku)
+         where 
+          newSudoku = [solve' (update sudoku p c) ps | c <- candidates sudoku p]
+
+
+readAndSolve :: FilePath -> IO()
+readAndSolve path = do a <- (readSudoku path)
+                       let sudoku = solve a
+                       if isNothing sudoku
+                        then error "could not solve sudoku"
+                       else printSudoku (fromJust sudoku)
+
+isSolutionOf :: Sudoku -> Sudoku -> Bool
+isSolutionOf start solution = --isOkay solution && 
+                              --isFilled solution &&
+                              and [fst c == snd c || isNothing (fst c) | c <- b]
+                  where b = zip
+                            (concat [a | a <- (extractRow start)])
+                            (concat [a | a <- (extractRow solution)])
